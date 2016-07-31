914ddefd59e6707ac5edde088fb679a305807670
ricomen <ricomen@mail.ru>
2016-07-31 08:38:48 +0500

    Подключил JS

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,16 @@
-var link = document.querySelector(".user-block");
+
+
+var  link = document.querySelector(".login");
+var popup = document.querySelector(".modal-content");
+var close = document.querySelector(".modal-content-close");
+
+	link.addEventListener("click", function(event) 
+	{
+		event.preventDefault();
+		popup.classList.add("modal-content-show");
+	});
+
+	close.addEventListener("click", function(event) {
+		event.preventDefault();
+		popup.classList.remove("modal-content-show");
+	});
